1cc761a134eb92f049bdfc3a5a2d50983b2ae61e
Ethan <ext8jv@virginia.edu>
2018-02-15 19:49:52 -0500

    adding many to one relation for feedback

--- app/test_project/project2/models.py
+++ app/test_project/project2/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from . import fields
 
 # Create your models here.
 class Song(models.Model):
@@ -14,12 +15,6 @@ class Music_Video(models.Model):
     artists = models.CharField(max_length=50, default="NO_ARTIST")
     owner = models.ForeignKey(User)
     video_file = models.FileField()
-    time_posted = models.TimeField()
-
-class Feedback(models.Model):
-    owner = models.ForeignKey(User)
-    ratings = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
-    comment = models.TextField()
     time_posted = models.TimeField()
 
 class Image(models.Model):
@@ -42,3 +37,14 @@ class Poem(models.Model):
     owner = models.ForeignKey(User)
     text = models.TextField()
     time_posted = models.TimeField()
+
+class Feedback(models.Model):
+    owner = models.ForeignKey(User)
+    ratings = fields.IntegerRangeField(min_value=0, max_value=5)
+    comment = models.TextField()
+    time_posted = models.TimeField()
+    song = models.ForeignKey(Song, default=None)
+    music_video = models.ForeignKey(Music_Video, default=None)
+    image = models.ForeignKey(Image, default=None)
+    story = models.ForeignKey(Story, default=None)
+    poem = models.ForeignKey(Poem, default=None)
