7efbdaec87bc8bd2bf29e03376323e3504ae7a2e
Sahana Arkalgud <sahanaarkalgud@d-172-25-254-222.dhcp.virginia.edu>
2018-02-18 21:31:34 -0500

    Added Music Video and Poem API

--- app/test_project/project2/serializers.py
+++ app/test_project/project2/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from .models import Song
+from .models import Song, Music_Video, Poem
 
 class SongSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Song
+		fields = ('id', 'title', 'artists', 'time_posted')
+		read_only_fields = ('time_posted', 'time_posted')
+
+class Music_Video_Serializer(serializers.ModelSerializer):
+	class Meta:
+		model = Music_Video
+		fields = ('id', 'title', 'artists', 'time_posted')
+		read_only_fields = ('time_posted', 'time_posted')
+
+class Poem_Serializer(serializers.ModelSerializer):
+	class Meta:
+		model = Poem
 		fields = ('id', 'title', 'artists', 'time_posted')
 		read_only_fields = ('time_posted', 'time_posted')
--- app/test_project/project2/views.py
+++ app/test_project/project2/views.py
@@ -2,8 +2,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
-from .serializers import SongSerializer
-from .models import Song
+from .serializers import SongSerializer, Music_Video_Serializer, Poem_Serializer
+from .models import Song, Music_Video, Poem
 from rest_framework.documentation import include_docs_urls
 
 def index(request):
@@ -28,5 +28,41 @@ class DetailsView(generics.RetrieveUpdateDestroyAPIView):
         song = Song.objects.get(pk=pk)
         song.delete()
 
+class Music_Video_CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Music_Video.objects.all()
+    serializer_class = Music_Video_Serializer
 
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
+
+class Music_Video_DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Music_Video.objects.all()
+    serializer_class = Music_Video_Serializer
+
+    def delete(self, request, pk, format=None):
+        vid = Music_Video.objects.get(pk=pk)
+        vid.delete()
+
+class Poem_CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Poem.objects.all()
+    serializer_class = Poem_Serializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
+
+class Poem_DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles the http GET, PUT and DELETE requests."""
+
+    queryset = Poem.objects.all()
+    serializer_class = Poem_Serializer
+
+    def delete(self, request, pk, format=None):
+        poem = Poem.objects.get(pk=pk)
+        poem.delete()
 
