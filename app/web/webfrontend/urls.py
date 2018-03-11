e5db6fe338e351f45998a94f9fd49e9d621217ac
sjl5bz <sjl5bz@virginia.edu>
2018-03-11 11:53:23 -0400

    added pages for all of the models. added base_generic.html

--- app/web/webfrontend/urls.py
+++ app/web/webfrontend/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^songs/$', views.songs, name='songs'),
+    url(r'^music_videos/$', views.music_videos, name='music_videos'),
+    url(r'^stories/$', views.stories, name='stories'),
+    url(r'^feedbacks/$', views.feedbacks, name='feedbacks'),
+    url(r'^images/$', views.images, name='images'),
+    url(r'^poems/$', views.poems, name='poems'),
 ]
--- app/web/webfrontend/views.py
+++ app/web/webfrontend/views.py
@@ -1,5 +1,95 @@
 from django.shortcuts import render
+import urllib.request, json
+from urllib.request import urlopen
+from django.http import JsonResponse
+
+
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def songs(request):
+    #retreiving data from url and converting to json format
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/songs/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    json_data = json.loads(resp_json)
+
+    #parsing json data
+
+    for element in json_data['results']:
+        artist = element['artists']
+        owner = element['owner']
+        title = element['title']
+        time_posted = element['time_posted']
+    return render(
+        request,
+        'songs_homepage.html',
+        context={'data': json_data['results']}
+    )
+
+def music_videos(request):
+    # retreiving data from url and converting to json format
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/songs/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    json_data = json.loads(resp_json)
+
+    return render(
+        request,
+        'music_videos_homepage.html',
+        context={'data': json_data['results']}
+    )
+
+def stories(request):
+    # retreiving data from url and converting to json format
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/stories/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    json_data = json.loads(resp_json)
+
+    return render(
+        request,
+        'stories_homepage.html',
+        context={'data': json_data['results']}
+    )
+
+def feedbacks(request):
+    # retreiving data from url and converting to json format
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/feedbacks/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    json_data = json.loads(resp_json)
+
+    return render(
+        request,
+        'feedbacks_homepage.html',
+        context={'data': json_data['results']}
+    )
+
+def images(request):
+    # retreiving data from url and converting to json format
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/images/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    json_data = json.loads(resp_json)
+
+    return render(
+        request,
+        'images_homepage.html',
+        context={'data': json_data['results']}
+    )
+
+def poems(request):
+    # retreiving data from url and converting to json format
+
+    req = urllib.request.Request('http://exp-api:8000/api/v1/poems/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    json_data = json.loads(resp_json)
+
+    return render(
+        request,
+        'poems_homepage.html',
+        context={'data': json_data['results']}
+    )
