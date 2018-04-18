d3df09b82dd909bb7f89a0f74b7fa47e030f78a5
Ethan <ext8jv@virginia.edu>
2018-04-17 20:55:50 -0400

    kafka and es indexing should work for all listings

--- app/exp/api/models_api.py
+++ app/exp/api/models_api.py
@@ -82,7 +82,9 @@ def get_image(pk):
 def add_image(image_dict):
     payload = image_dict
     response = requests.post('http://models-api:8000/project2/api/v1/images/create/', data=payload)
-    kafka_api.send_to_kafka(response.json())
+    json_data = response.json()
+    json_data["type"] = "image"
+    kafka_api.send_to_kafka(json_data)
     return response.json()
 
 def get_stories(page):
@@ -115,7 +117,9 @@ def get_story(pk):
 def add_story(story_dict):
     payload = story_dict
     response = requests.post('http://models-api:8000/project2/api/v1/story/create/', data=payload)
-    kafka_api.send_to_kafka(response.json())
+    json_data = response.json()
+    json_data["type"] = "story"
+    kafka_api.send_to_kafka(json_data)
     return response.json()
 
 def get_music_videos(page):
@@ -148,7 +152,9 @@ def get_music_video(pk):
 def add_music_video(music_vid_dict):
     payload = music_vid_dict
     response = requests.post('http://models-api:8000/project2/api/v1/music_videos/create/', data=payload)
-    kafka_api.send_to_kafka(response.json())
+    json_data = response.json()
+    json_data["type"] = "music_video"
+    kafka_api.send_to_kafka(json_data)
     return response.json()
 
 def get_poems(page):
@@ -181,7 +187,9 @@ def get_poem(pk):
 def add_poem(poem_dict):
     payload = poem_dict
     response = requests.post('http://models-api:8000/project2/api/v1/poems/create/', data=payload)
-    kafka_api.send_to_kafka(response.json())
+    json_data = response.json()
+    json_data["type"] = "poem"
+    kafka_api.send_to_kafka(json_data)
     return response.json()
 
 def get_users(page):
