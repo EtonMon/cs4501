460bb1ccdd05719427d37945cbee124f7e530771
Ethan <ext8jv@virginia.edu>
2018-04-23 18:27:34 -0400

    adding mysql setup script

---
+++ scripts/setup-mysql.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -uroot -p'\$3cureUS' -h db --execute "create user 'www'@'%' identified by '\$3cureUS'; create database cs4501 character set utf8; grant all on cs4501.* to 'www'@'%';"
