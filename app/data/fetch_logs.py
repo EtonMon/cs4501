d6aea658c20ed51fdad45bc453ad27f0dd3fad32
Ethan <ext8jv@virginia.edu>
2018-04-25 23:10:48 -0400

    setting up containers for spark

---
+++ app/data/fetch_logs.py
@@ -0,0 +1,25 @@
+from kafka import KafkaConsumer
+import json
+import time
+
+# Attempting to establish with kafka every second for 20 seconds
+timeout = 20
+for i in range(timeout):
+    time.sleep(1)
+    try:
+        print("----------------ATTEMPTING TO CONNECT TO SPARK-KAFKA----------------------", flush=True)
+        consumer = KafkaConsumer('new-log-topic', group_id='log-indexer', bootstrap_servers=['spark-kafka:9092'])
+        print("----------------SUCCESSFULLY CONNECTED TO SPARK-KAFKA----------------------", flush=True)
+        break
+    except:
+        print("----------------CONNECTION TO SPARK-KAFKA FAILED----------------------", flush=True)
+
+# Attempts to process messages from kafka if connection established
+try:
+    print("----------------ATTEMPTING TO LISTEN FOR NEW LOGS----------------------", flush=True)
+    for message in consumer:
+        print("log received",flush=True)
+        new_log = json.loads((message.value).decode('utf-8'))
+        print(new_log,flush=True)
+except:
+    print("----------------SPARK-KAFKA CONNECTION NEVER SUCCESSFULLY ESTABLISHED----------------------", flush=True)
